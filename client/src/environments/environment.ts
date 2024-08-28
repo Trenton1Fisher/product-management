7f34ab9c96264fc0690a72927af5255478afbbc5
Trenton Fisher <102687844+Trenton1Fisher@users.noreply.github.com>
2024-08-28 02:06:39 -0700

    Update environment.ts

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,3 +1,4 @@
+//Supabase keys are fine to show as long as row level security is setup properly according to supabase angualar setup docs
 export const environment = {
   production: false,
   supabaseUrl: 'https://qvrvtczpanfdvxgixenk.supabase.co',
